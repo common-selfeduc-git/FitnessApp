640bb74dc6003472598ff8de4be4fedc8fac95d4
Leontij <mrleonchik@gmail.com>
2022-11-22 12:22:57 +0300

    Донастроен класс User

--- FitnessApp/Fitness.BL/Model/Gender.cs
+++ FitnessApp/Fitness.BL/Model/Gender.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Fitness.BL.Model
 {
-    internal class Gender
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
     {
+        /// <summary>
+        /// Название.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создать новый пол.
+        /// </summary>
+        /// <param name="name">Имя пола.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null.", nameof(name));
+            }
+
+            Name = name;
+
+        }
+        
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
